d384fc9930741e83b6671e60fa6d9c956feb4687
Yashih Wu <yashih.wu@gmail.com>
2018-09-13 03:26:40 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,7 @@ def loop_iterator(number_of_times)
     end
 end
 
-def times_iterator
+def times_iterator(number_of_times)
   7.times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
