0577d28932a3f35eb2e02dd4a52575439b0a4b70
Yashih Wu <yashih.wu@gmail.com>
2018-09-13 03:00:18 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,11 +1,11 @@
 
-number_of_times = 1
+number_of_times = 0
 
 def loop_iterator(number_of_times)
     loop do
-      puts "Welcome to Flatiron School's Web Development Course!"
+      print "Welcome to Flatiron School's Web Development Course!"
       number_of_times += 1
-      if number_of_times > 7
+      if number_of_times >= 7
         break
       end
     end
